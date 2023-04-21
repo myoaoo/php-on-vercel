8a4ed10ee7e4f5de1690730e6be9bf7838abd150
Angelo Geulin <angelogeulin123@gmail.com>
2023-04-22 01:55:27 +0800

    chore: add instructions on how to get file-based routing working

--- api/index.php
+++ api/index.php
@@ -8,8 +8,29 @@
   <title>PHP on Vercel</title>
 </head>
 <body>
-  <?php
-    echo 'Hello from PHP!';
-  ?>
+  <h1>
+    <?php
+      echo 'Hello from Vercel PHP!';
+    ?>
+  </h1>
+  <p>It is possible to get file-based routing working on the community <a href="https://github.com/vercel-community/php">Vercel Functions PHP runtime</a>.</p>
+  <p>You simply need the following configuration:</p>
+  <code>vercel.json</code>
+  <pre>
+{
+  "functions": {
+    "api/*.php": {
+      "runtime": "vercel-php@0.6.0"
+    }
+  },
+  "routes": [{ "src": "/(.*)", "dest": "/api/$1" }]
+}
+  </pre>
+  <p>And, make sure to place all your PHP files under <code>/api</code>.</p>
+  <nav>
+    <a href="index.php">Go to Index</a>
+    <a href="page-two.php">Go to Page 2</a>
+    <a href="php-info.php">View PHP Info</a>
+  </nav>
 </body>
 </html>
