02f520b6ab2dbcb80957718ab5336c2ea30d6e03
myoaoo <ssjia.arron@gmail.com>
2025-05-09 17:37:20 +0800

    2

--- api/index.php
+++ api/index.php
@@ -36,7 +36,7 @@ $logged_in = isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP on Vercel</title>
+  <title>林氏成员信息库</title>
 </head>
 <body>
   <?php if ($logged_in): ?>
