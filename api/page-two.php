7e1aa07784d856833f5c6c3f464163417fbf7c10
myoaoo <ssjia.arron@gmail.com>
2025-05-09 08:29:55 +0800

    2

--- api/page-two.php
+++ api/page-two.php
@@ -13,13 +13,12 @@
     ?>
   </h1>
   <p>This is page two.</p>
-  <img src="../source/images/example1.jpg" alt="Example Image 1">
-  <img src="./source/images/12.png" alt="Example Image 2">
-  <img src="/source/images/12.png" alt="Example Image 3">
-  <img src="source/images/12.png" alt="Example Image 4">
+<!-- 推荐这3种写法 -->
+<img src="/source/images/12.png" alt="正确路径1">
+<img src="source/images/12.png" alt="正确路径2"> 
+<img src="/api/source/images/12.png" alt="正确路径3">
   <img src="https://www.oaoo.top/assets/images/post/3-1.png" alt="Example Image 5">
-  <img src="api/source/images/12.png" alt="Example Image 6">
-  <img src="/api/source/images/12.png" alt="Example Image 7">
+
   <nav>
     <a href="index.php">Go to Index</a>
     <a href="page-two.php">Go to Page 2</a>
